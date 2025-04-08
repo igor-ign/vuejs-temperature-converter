41a59890afd688b0f16617c14781ae64649e5fc8
Igor Ignácio <euleiodps@gmail.com>
2025-04-08 14:18:05 -0300

    #2 - Feat: temperature converter input

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import { allowOnlyNumbers } from './helpers/allow-only-numbers'
 
-<template></template>
+const celsiusTemperature = ref<string>('')
+
+function handleInputChange(event: Event) {
+  const { value } = event.target as HTMLInputElement
+  celsiusTemperature.value = allowOnlyNumbers(value)
+}
+
+function convertCelsiusToFahrenheit(celsius: number): number {
+  return (celsius * 9) / 5 + 32
+}
+
+const message = computed(() =>
+  celsiusTemperature.value.length === 0
+    ? 'Type a temperature in Celsius to convert it to Fahrenheit'
+    : `The temperature ${celsiusTemperature.value} converted to fahrenheit is ${convertCelsiusToFahrenheit(Number(celsiusTemperature.value))}`,
+)
+</script>
+
+<template>
+  <input type="text" @input="handleInputChange" v-model="celsiusTemperature" />
+  <span>{{ message }}</span>
+</template>
 
 <style scoped></style>
